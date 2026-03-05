33517b6b1902395e04abd6b814854aef21c5c1bb
CHILLA01 <tchilaiagiorgi@gmail.com>
2026-03-05 18:25:12 +0400

    Add Store project, remove old shop files

--- shop/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Category, ProductModel
-
-
-admin.site.register(Category)
-admin.site.register(ProductModel)
--- shop/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import ProductModel
-
-def product_list(request):
-    products = ProductModel.objects.all()
-    return render(request, 'products.html', {'products': products})
